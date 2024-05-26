26b046673f0588049d18ed0ac8bb22b7079ca826
parkyeomyeong <tldldh1212@naver.com>
2024-05-26 17:44:44 +0900

    feat$(config): CORS 에러 방지를 위해 Front 서버 도메인 허용 Config 취합

--- src/main/java/com/upgrade/meoku/config/MeokuAppConfig.java
+++ src/main/java/com/upgrade/meoku/config/MeokuAppConfig.java
@@ -4,11 +4,23 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MeokuAppConfig {
+public class MeokuAppConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    // CORS 에러 방지를 위해 front domain 허용(front 로컬, front 운영)
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "https://front-kappa-lac.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
