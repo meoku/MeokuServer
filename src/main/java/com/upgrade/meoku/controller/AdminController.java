825df8818296409b72cb6e6851444a0fc0146ef6
parkyeomyeong <tldldh1212@naver.com>
2024-06-08 16:01:24 +0900

    feat$(application): Cloud서버 시간이 8 시간 늦은 관계로 설정으로 명시 및 healcheck 로직 추가

--- src/main/java/com/upgrade/meoku/controller/AdminController.java
+++ src/main/java/com/upgrade/meoku/controller/AdminController.java
@@ -2,15 +2,21 @@ package com.upgrade.meoku.controller;
 
 import com.upgrade.meoku.data.dto.MeokuDailyMenuDTO;
 import com.upgrade.meoku.exception.MeokuException;
+import com.upgrade.meoku.schedule.WeatherData;
 import com.upgrade.meoku.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Tag(name = "관리자 관련 컨트롤러", description = "Meoku 관리를 위한 관리자 요청")
 @RestController
@@ -27,8 +33,10 @@ public class AdminController {
     @Operation(summary = "health check")
     @GetMapping(value = "/healthCheck")
     @ResponseBody
-    public String healthCheck() throws Exception {
-        return "OK";
+    public ResponseEntity<Map<String, Object>> healthCheck() throws Exception {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("responseBody", LocalDate.now().toString());
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
     @Operation(summary = "식단 이미지 OCR", description = "업로드된 이미지 or 파일을 받아 API를 이용해 식단 데이터를 반환한다")
