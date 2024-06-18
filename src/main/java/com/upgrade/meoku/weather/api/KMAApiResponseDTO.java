26532ab31d8cfcb747c9c371fe296c939fe8c25c
박여명 <68410186+parkyeomyeong@users.noreply.github.com>
2024-06-18 14:03:48 +0900

    fix(DTO) : 날씨 API 호출 응답값 초단기실황 필드부분 추가

--- src/main/java/com/upgrade/meoku/weather/api/KMAApiResponseDTO.java
+++ src/main/java/com/upgrade/meoku/weather/api/KMAApiResponseDTO.java
@@ -4,13 +4,16 @@ import lombok.Data;
 
 @Data
 public class KMAApiResponseDTO {
-    //단기에보 응답값
+    //공통
     private String baseDate;
     private String baseTime;
     private String category;
+    private int nx;
+    private int ny;
+    //초단기실황
+    private String obsrValue;
+    //단기에보 응답값
     private String fcstDate;
     private String fcstTime;
     private String fcstValue;
-    private int nx;
-    private int ny;
 }
